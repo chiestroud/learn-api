4519d8afe3b3676a72ae8167fcd18c5ad3b6a912
Chie Stroud <urirunrunkoko@gmail.com>
2021-03-23 20:52:18 -0500

    adding weather API

--- src/javascripts/events/domEvents.js
+++ src/javascripts/events/domEvents.js
@@ -1,8 +1,10 @@
 import displayJokeContainer from '../components/forms/jokeContainer';
 import showJoke from '../components/forms/showJoke';
 import showLyrics from '../components/forms/showLyrics';
+import showWeather from '../components/forms/showWeather';
 import getJoke from '../helpers/data/jokeData';
 import getLyrics from '../helpers/data/lyricsData';
+import getWeather from '../helpers/data/weatherData';
 
 const domEvents = () => {
   document.querySelector('body').addEventListener('click', (e) => {
@@ -23,6 +25,11 @@ const domEvents = () => {
       const songTitle = document.querySelector('#songTitle').value;
       document.querySelector('#searchLyrics').innerHTML = 'Loading...';
       getLyrics(artist, songTitle).then((songObject) => showLyrics(songObject));
+    }
+    if (e.target.id.includes('searchWeather')) {
+      e.preventDefault();
+      const city = document.querySelector('#location').value;
+      getWeather(city).then((weatherObject) => showWeather(weatherObject));
     }
   });
 };
